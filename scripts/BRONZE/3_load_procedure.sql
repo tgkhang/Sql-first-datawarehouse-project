48b8ecae302b06789371d192a06d5ab964627f5f
khang <tgkhang22@clc.fitus.edu.vn>
2025-05-28 00:50:49 +0700

    [m] Adding notification log

--- scripts/BRONZE/3_load_procedure.sql
+++ scripts/BRONZE/3_load_procedure.sql
@@ -5,7 +5,18 @@ go
 create or alter procedure bronze.load_bronze
 as
 BEGIN
+	PRINT '================================================';
+	PRINT 'Loading Bronze Layer';
+	PRINT '================================================';
+
+	PRINT '------------------------------------------------';
+	PRINT 'Loading CRM Tables';
+	PRINT '------------------------------------------------';
+
+
+	PRINT '>> Truncating Table: bronze.crm_cust_info';
 	truncate table bronze.crm_cust_info
+	PRINT '>> Inserting Data Into: bronze.crm_cust_info';
 	bulk insert bronze.crm_cust_info
 	from 'C:\PERSONAL PAGE\REPO\Sql-first-datawarehouse-project\datasets\source_crm\cust_info.csv'
 	with (
@@ -18,7 +29,9 @@ BEGIN
 	--SELECT * from bronze.crm_cust_info
 	--SELECT count(*) from bronze.crm_cust_info
 
+	PRINT '>> Truncating Table: bronze.crm_prd_info';
 	truncate table bronze.crm_prd_info
+	PRINT '>> Inserting Data Into: bronze.crm_prd_info';
 	bulk insert bronze.crm_prd_info
 	from 'C:\PERSONAL PAGE\REPO\Sql-first-datawarehouse-project\datasets\source_crm\prd_info.csv'
 	with (
@@ -29,7 +42,9 @@ BEGIN
 	);
 	--select * from bronze.crm_prd_info
 
+	PRINT '>> Truncating Table: bronze.crm_sales_details';
 	truncate table bronze.crm_sales_details
+	PRINT '>> Inserting Data into Table: bronze.crm_sales_details';
 	bulk insert bronze.crm_sales_details
 	from 'C:\PERSONAL PAGE\REPO\Sql-first-datawarehouse-project\datasets\source_crm\sales_details.csv'
 	with (
@@ -40,7 +55,14 @@ BEGIN
 	);
 	--select * from bronze.crm_sales_details
 
+
+	PRINT '------------------------------------------------';
+	PRINT 'Loading ERP Tables';
+	PRINT '------------------------------------------------';
+
+	PRINT '>> Truncating Table: bronze.erp_cust_az12';
 	truncate table bronze.erp_cust_az12
+	PRINT '>> Insert Data into Table: bronze.erp_cust_az12';
 	bulk insert bronze.erp_cust_az12
 	from 'C:\PERSONAL PAGE\REPO\Sql-first-datawarehouse-project\datasets\source_erp\CUST_AZ12.csv'
 	with (
@@ -51,7 +73,9 @@ BEGIN
 	);
 	--select * from bronze.erp_cust_az12
 
+	PRINT '>> Truncating Table: bronze.erp_loc_a101';
 	truncate table bronze.erp_loc_a101
+	PRINT '>> Insert Data into Table: bronze.erp_loc_a101';
 	bulk insert bronze.erp_loc_a101
 	from 'C:\PERSONAL PAGE\REPO\Sql-first-datawarehouse-project\datasets\source_erp\LOC_A101.csv'
 	with (
@@ -62,8 +86,9 @@ BEGIN
 	);
 	--select * from bronze.erp_loc_a101
 
-
+	PRINT '>> Truncating Table: bronze.erp_px_cat_g1v2';
 	truncate table bronze.erp_px_cat_g1v2
+	PRINT '>> Insert Data into Table: bronze.erp_px_cat_g1v2';
 	bulk insert bronze.erp_px_cat_g1v2
 	from 'C:\PERSONAL PAGE\REPO\Sql-first-datawarehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
 	with (
