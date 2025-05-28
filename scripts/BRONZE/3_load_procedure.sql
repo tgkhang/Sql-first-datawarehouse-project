45949dd63e737b26581ca2675b1fdecf8355e0ce
khang <tgkhang22@clc.fitus.edu.vn>
2025-05-28 10:55:38 +0700

    [+] Create table script for silver

--- scripts/BRONZE/3_load_procedure.sql
+++ scripts/BRONZE/3_load_procedure.sql
@@ -1,3 +1,20 @@
+/*
+===============================================================================
+Stored Procedure: Load Bronze Layer (Source -> Bronze)
+===============================================================================
+Script Purpose:
+    This stored procedure loads data into the 'bronze' schema from external CSV files. 
+    It performs the following actions:
+    - Truncates the bronze tables before loading data.
+    - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
+
+Parameters:
+    None. 
+Usage :
+    EXEC bronze.load_bronze;
+===============================================================================
+*/
+
 use Datawarehouse;
 go
 
